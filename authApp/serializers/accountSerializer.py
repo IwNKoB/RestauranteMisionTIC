239469196fdb3820a458f7e76f747d288329f7b4
Ivan <iwn172@gmail.com>
2022-07-18 21:15:00 -0500

    deployement changes

--- authApp/serializers/accountSerializer.py
+++ authApp/serializers/accountSerializer.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model=Account
-        fields=['balance','lastChangeData','isActive']
+        fields=['balance','lastChangeDate','isActive']
